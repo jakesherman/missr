372b766fa61ac7cdfa0219cff744425a4c41f67f
Jake Sherman <jake@jakesherman.com>
2015-11-11 22:17:09 -0500

    Reexporting the magrittr pipe

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,19 @@
 ## ============================================================================
 ##
 ## Utilities
+## - reexporting the magrittr pipe - code copied from hadley/purrr
 ##
 ## ============================================================================
+
+#' Pipe operator
+#'
+#' @name %>%
+#' @rdname pipe
+#' @keywords internal
+#' @export
+#' @importFrom magrittr %>%
+#' @usage lhs \%>\% rhs
+NULL
 
 as_percent <- function(num, total, dig = 1) {
     
