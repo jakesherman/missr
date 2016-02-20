396a4c1933847e1605574ffe3bfa60c82701e478
Jake Sherman <jake@jakesherman.com>
2016-02-20 09:06:15 -0500

    Added a bunch of ideas around how to implement the `fill_na` functionb

--- R/fill_na.R
+++ R/fill_na.R
@@ -6,6 +6,108 @@
 ##
 ## ============================================================================
 
+# Syntax wise, which do we like better?
+
+# Option 1: have the functions apply to vectors and use functions that apply
+# functions to vectors (map, lapply) to do a list of vectors (data.frame)
+mydata <- mydata %>%
+    map_if(is.integer, fill_na, -999) %>%
+    map_if(is.character, fill_na, "na") %>%
+    mutate(SpecificColumn = fill_na(SpecificColumn, 3.14159)) %>% # OR
+    map_at("SpecificColumn", fill_na, 3.14159) %>%
+    map(fill_na, "X")
+
+# Option 2: existing functionality
+mydata <- mydata %>%
+    fill_na(-999, is.numeric) %>%
+    fill_na("na", is.character) %>%
+    fill_na(3.14159, SpecificColumn) %>%
+    fill_na("X")
+
+# Option 3: take option 2 by purrr it by making different functions
+mydata <- mydata %>%
+    fill_na_if(is.numeric, -999) %>%
+    fill_na_if(is.character, "na") %>%
+    fill_na_cols(SpecificColumn, 3.14159) %>% # -- dont think this will work
+    fill_na("X")
+
+# Option 4: like option 3 but w/ args turned around
+mydata <- mydata %>%
+    fill_na_if(-999, is.numeric) %>%
+    fill_na_if("na", is.character) %>%
+    fill_na_cols(3.14159, SpecificColumn) %>%
+    fill_na("X")
+
+# Biggest problem of combining NSE column selection with everything else is
+# that we can't give addtional arguments to .fill with ..., BUT, how often
+# would that be needed? Alternatives (ex. for mean) are:
+# 1) create new functions, mean2 <- function(x) mean(x, na.rm = TRUE)
+# 2) Anonymous functions, ex. function(f) mean(x, na.rm = TRUE)
+# 3) Easily do wither with purrr::partial, ex. partial(mean, na.rm = TRUE)
+
+# Ex. fill in NAs with column means, ... are args
+mydata <- mydata %>%
+    fill_na(mean, na.rm = TRUE)
+
+# Applying the above to a specific column means no NSE, right?
+mydata <- mydata %>%
+    fill_na_at("SpecificColumn", mean, na.rm = TRUE)
+
+# Try the above but using an anonymous function and ... is NSE
+mydata <- mydata %>%
+    fill_na(partial(mean, na.rm = TRUE), SpecificColumn)
+mydata <- mydata %>%
+    fill_na(function(f) mean(f, na.rm = TRUE), SpecificColumn)
+
+# Does having separate functions alleviate this at all?
+mydata <- mydata %>%
+    fill_na_mean(SpecificColumn)
+
+# NSE makes filling everything BUT one column much easier, ex.
+mydata <- mydata %>%
+    fill_na_at(c("OtherColumn1", "OtherCol2", "OtherCol3"), mean, na.rm = TRUE)
+
+# ...versus...
+mydata <- mydata %>%
+    fill_na(partial(mean, na.rm = TRUE), -SpecificColumn)
+
+# OR
+mydata <- mydata %>%
+    fill_na_mean(-SpecificColumn)
+
+# What about a compromise where we use NSE for column selection, and a .args
+# argument as a list of arguments to .f?
+mydata <- mydata %>%
+    fill_na(mean, -SpecificColumn, .args = list(na.rm = TRUE))
+
+# How useful are function inputs and having ... be the arguments to .f?
+fill_na_mean
+fill_na_median
+fill_na_mode
+fill_na_default # -- mean for numerics, mode for categories
+
+# How useful are predicate functions? We could also create multiple functions
+# that do the most common predicates, such as
+fill_na_int
+fill_na_lgl
+fill_na_dbl
+fill_na_chr
+
+# So that instead of 
+mydata <- mydata %>%
+    fill_na_if(is.double, 3.14159)
+
+# Do:
+mydata <- mydata %>%
+    fill_na_dbl(3.14159)
+
+# How useful are predicate functions if we bypass R's coersion rules and
+# only apply fill_na to columns of type .fill? In that case:
+mydata <- mydata %>%
+    fill_na(3.14159)
+
+# Only gets is.double columns, not is.integer, is.character, etc. columns
+
 #' cols_of_type()
 #' 
 #' Identifies which columns in a data.frame are of a specific class.
@@ -71,7 +173,6 @@ fill_na_.data.frame <- function(.data, fill, ..., .dots) {
         
         # If fill is a function, loop over vars and assign all NAs in the col
         # corresponding to each var with the result of the function /////
-        
         for (var in vars) {
             fill_value <- fill(.data[[var]])
             if (length(fill_value) != 1) {
@@ -87,7 +188,6 @@ fill_na_.data.frame <- function(.data, fill, ..., .dots) {
     } else if (is.list(fill) && !is.null(names(fill))) {
         
         # If fill is a named list, loop over the columns /////
-        
         if (!all(names(fill) %in% vars)) {
             
             # If the inputted columns aren't valid, return an error
@@ -118,7 +218,6 @@ fill_na_.data.frame <- function(.data, fill, ..., .dots) {
         
         # If fill is anything else, loop over that thing and fill in NAs for
         # columns with matching types
-        
         fill_types <- vapply(fill, class, character(1))
         if (any(table(fill_types) > 1)) {
             stop("You entered more than one fill value for a given class")
--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,6 @@
 ## ============================================================================
 ##
-## Utilities
-## - reexporting the magrittr pipe - code copied from hadley/purrr
+## Utilities - common functions, reexported functions, etc.
 ##
 ## ============================================================================
 
@@ -20,3 +19,32 @@ as_percent <- function(num, total, dig = 1) {
     # Returns (num)/(total) as a character percentage
     paste0(round((num * 100) / total, dig), "%")
 }
+
+create_random_na_vector <- function(vector, num_NAs) {
+    
+    # Randomly select #[num_NAs] indicies from [vector] and turn them into NAs
+    na_indicies <- sample(seq(length(vector)), num_NAs)
+    vector[seq(length(vector)) %in% na_indicies] <- NA
+    vector
+}
+
+create_random_na <- function(data, prop_na_range = c(.25, .75), 
+                             ignore_cols = NULL) {
+    
+    # Given a dataframe [data], create a random number of NA values in each 
+    # column not in [ignore_cols] with proportions ranging from the beginning
+    # value of [prop_na_range] to the last value of it
+    data <- data
+    cols <- names(data)
+    if (!is.null(ignore_cols)) 
+        cols <- cols[!cols %in% ignore_cols]
+    col_props <- round(
+        runif(length(cols), prop_na_range[1], prop_na_range[2]) * nrow(data), 1)
+    for (i in seq_along(cols)) {
+        data[[cols[i]]] <- create_random_na_vector(
+            data[[cols[i]]], col_props[i])
+    }
+    
+    data
+}
+
